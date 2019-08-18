66addbd3b8d11e5c66a62ebea9254e593a4f0be6
League <league@iMac.attlocal.net>
2019-08-18 11:27:18 -0700

    Continued working on client greeter! :)

--- src/_02_Chat_Application/ChatApp.java
+++ src/_02_Chat_Application/ChatApp.java
@@ -1,9 +1,23 @@
 package _02_Chat_Application;
 
+import _00_Click_Chat.networking.Client;
+import _00_Click_Chat.networking.Server;
+
 /*
  * Using the Click_Chat example, write an application that allows a server computer to chat with a client computer.
  */
 
 public class ChatApp {
 
+	
+	
+	public static void main(String[] args) {
+		
+		Server server = new Server(3478);
+		int port = server.getPort();
+		String ip = server.getIPAddress();
+		Client client = new Client(ip, port);
+		
+		
+	}
 }
